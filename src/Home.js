38d60137c00ae7b6814edeac8730029ad69ed4e2
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-28 22:29:09 +0530

    Basic styles added

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='Home'>
+      <h2>Homepage</h2>
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default Home
