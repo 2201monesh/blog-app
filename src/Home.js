5787d0606619f4d084bb3c84824eac83dce59259
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-07-05 17:21:51 +0530

    added error handling in Home

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,30 @@ function Home() {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setISPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        if(!res.ok){
+          throw Error("Could not fetch the data rn");
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data)
         setISPending(false)
+        setError(null)
+      })
+      .catch(err => {
+        setISPending(false);
+        setError(err.message);
       })
     }, []);
 
   return (
     <div className='Home'>
+      { error && <div>{ error } </div>}
       {isPending && <div>Loading...</div>}
       { blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
